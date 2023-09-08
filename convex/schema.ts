3375752c44dc416bd5430146def675b9b7c026ea
Salmandotweb <salmandotweb@gmail.com>
2023-09-09 02:16:54 +0500

    changing schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,5 +2,8 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    blueprints: defineTable({ prompt: v.string(), result: v.optional(v.string()), }),
+    blueprints: defineTable({
+        prompt: v.string(),
+        result: v.optional(v.string()),
+    }),
 });
