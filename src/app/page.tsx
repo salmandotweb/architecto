4d627b606a57cf969870c6eb8081cad17e4cb983
Salmandotweb <salmandotweb@gmail.com>
2023-09-09 16:34:08 +0500

    building sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import {
-	AuthLoading,
-	Authenticated,
-	Unauthenticated,
-	useMutation,
-	useQuery,
-} from "convex/react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { api } from "../../convex/_generated/api";
-import { SignInButton, SignOutButton } from "@clerk/clerk-react";
+import { AuthLoading, Authenticated, Unauthenticated } from "convex/react";
 
-type Inputs = {
-	prompt: string;
-};
+import { SignInButton } from "@clerk/clerk-react";
 
 export default function Home() {
-	const generateBluePrint = useMutation(api.blueprint.generateBluePrint);
-	const collection = useQuery(api.blueprint.getBluePrints);
-
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
-	} = useForm<Inputs>();
-	const onSubmit: SubmitHandler<Inputs> = (data) => {
-		return (
-			generateBluePrint({
-				prompt: data.prompt,
-			}),
-			reset()
-		);
-	};
-
 	return (
 		<div className="flex flex-col w-full items-center justify-center min-h-screen">
-			<Authenticated>
-				<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
-					<Input
-						{...register("prompt", {
-							required: true,
-						})}
-					/>
-					<Button type="submit">Submit</Button>
-					{errors.prompt && (
-						<span className="text-[#000] text-[12px]">
-							{errors.prompt.type === "required" && "Prompt is required"}
-						</span>
-					)}
-				</form>
-				<div className="flex flex-wrap gap-3">
-					{collection?.map((item) => {
-						return (
-							<div className="flex flex-col gap-2">
-								<div className="text-[#000] text-[12px]">{item.prompt}</div>
-								<img
-									key={item._id}
-									width="256"
-									height="256"
-									src={item.result}
-								/>
-							</div>
-						);
-					})}
-				</div>
-
-				<SignOutButton />
-			</Authenticated>
+			<Authenticated>Authenticated</Authenticated>
 			<Unauthenticated>
 				<SignInButton mode="modal" />
 			</Unauthenticated>
---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { RiRobot2Fill } from "react-icons/ri";
+import { Badge } from "../ui/badge";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { BsCollection } from "react-icons/bs";
+import { HiOutlineBuildingStorefront } from "react-icons/hi2";
+import { GiFallingStar } from "react-icons/gi";
+
+const sidebarLinks = [
+	{
+		name: "Dashboard",
+		icon: <LuLayoutDashboard />,
+		path: "/",
+	},
+	{
+		name: "Collection",
+		icon: <BsCollection />,
+		path: "/collection",
+	},
+	{
+		name: "Marketplace",
+		icon: <HiOutlineBuildingStorefront />,
+		path: "/marketplace",
+	},
+	{
+		name: "Dreamspace",
+		icon: <GiFallingStar />,
+		path: "/dreamspace",
+	},
+];
+
+const Sidebar = () => {
+	return (
+		<div className="flex flex-col gap-4 items-start justify-start min-h-screen min-w-[300px] border-r">
+			<div className="flex items-start justify-start p-6 border-b w-full">
+				<h1 className="text-2xl font-bold">Architecto</h1>
+			</div>
+			<div className="flex items-start justify-start p-6 border-b w-full">
+				<Button size="icon" className="w-full gap-2 py-6">
+					<RiRobot2Fill />
+					Build with AI
+					<Badge
+						variant="default"
+						className="bg-white hover:bg-white text-primary rounded-xl">
+						New
+					</Badge>
+				</Button>
+			</div>
+			<div className="flex items-start justify-start p-6 border-b w-full flex-col gap-3">
+				<h1 className="text-base text-[#5c5c5c] font-medium">Menu</h1>
+				<div className="flex flex-col items-start justify-start w-full gap-2">
+					{sidebarLinks.map((link, index) => {
+						return (
+							<Link
+								href={link.path}
+								key={index}
+								style={{
+									width: "100%",
+								}}>
+								<Button
+									size="icon"
+									variant="ghost"
+									className="w-full gap-2 py-5 flex items-center justify-start text-start text-[16px] pl-2">
+									{link.icon}
+									{link.name}
+								</Button>
+							</Link>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
